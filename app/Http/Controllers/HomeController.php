7d460e76345a00f77bb1c9146609133b03d27f96
Raynaldi Zulfikar <raynaldizoel@gmail.com>
2023-08-25 19:33:37 +0700

    Add Check Role

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $check = false;
+        $user = User::where('id', Auth::user()->id)->with('muzakki')->first();
+        $getJenis = $user->muzakki->jenis;
+
+        $exceptAttributeForPerorangan = [
+            'nama_pimpinan',
+            'nik_pimpinan',
+            'nama_cp',
+            'telp_cp'
+        ];
+
+        $tesTrue = [];
+        $tesFalse = [];
+
+        if($getJenis == 'perorangan'){
+            foreach($user->getAttributes() as $index => $atribute){
+                // jika atribute kecuali yang ada di array $exceptAttributeForPerorangan itu ada yang null, maka $check = false
+                if(in_array($index, $exceptAttributeForPerorangan)){
+                    if($user->$atribute === null){
+                        $check = false;
+                        array_push($tesTrue, $index);
+                    }else{
+                        $check = true;
+                        array_push($tesFalse, $index);
+                    }
+                }
+            }
+        }
+        // dd($tesTrue, $tesFalse);
+
+        // $requiredAttributes = [
+        //     'nama_pimpinan',
+        //     'nik_pimpinan',
+        //     'nama_cp',
+        //     'telp_cp'
+        // ];
+
+        // if ($getJenis != 'perorangan') {
+        //     foreach ($requiredAttributes as $attribute) {
+        //         if ($user->$attribute === null) {
+        //             $check = false;
+        //             break;
+        //         } else {
+        //             $check = true;
+        //         }
+        //     }
+        // } else {
+        //     $check = false;
+        // }
+
+        // dd($check);
+
+
+        return view('home.index', compact('check'));
     }
 
     public function profile()
